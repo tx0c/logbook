1768a75944eaf0d1fc4476bec91dc31a5e998445
bluecloud <96812901+pitb2022@users.noreply.github.com>
2022-03-04 17:54:52 +0800

    add book case stranger page

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -22,6 +22,7 @@ export * from "./IconAngleLeft";
 export * from "./IconAngleRight";
 export * from "./IconArrowRight";
 export * from "./IconBookcase";
+export * from './IconBookEye'
 export * from "./IconCheck";
 export * from "./IconClear";
 export * from "./IconDiscord";
